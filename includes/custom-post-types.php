de2fb9f7aeb82ca9d7d58c4ba409c31d27a8dd6a
Bohdan <zamchenco@gmail.com>
2017-06-20 18:26:59 +0300

    expand  custom post types template

--- includes/custom-post-types.php
+++ includes/custom-post-types.php
@@ -1,35 +1,60 @@
 <?php
-////////////////////////////////////////////////////////////////////  Post type
-add_action( 'init', 'post_type_news' );
-function post_type_news() {
+//////////Reviews
+add_action( 'init', 'post_type_review' );
+function post_type_review() {
   $labels = array(
-    'name'               => 'Новости',
-    'singular_name'      => 'Новость',
-    'add_new'            => 'Добавить',
-    'add_new_item'       => 'Добавить новость',
-    'edit_item'          => 'Редактировать',
-    'new_item'           => 'Добавить',
-    'view_item'          => 'Посмотреть',
-    'search_items'       => 'Поиск',
-    'not_found'          => 'Не найдено',
-    'not_found_in_trash' => 'Не найдено',
+    'name'               => 'Отзывы', // основное название для типа записи
+    'singular_name'      => 'Отзыв', // название для одной записи этого типа
+    'add_new'            => 'Добавить отзыв', // для добавления новой записи
+    'add_new_item'       => 'Добавление отзыва', // заголовка у вновь создаваемой записи в админ-панели.
+    'edit_item'          => 'Редактирование отзыва', // для редактирования типа записи
+    'new_item'           => 'Новый отзыв', // текст новой записи
+    'view_item'          => 'Смотреть отзыв', // для просмотра записи этого типа.
+    'search_items'       => 'Искать отзыв', // для поиска по этим типам записи
+    'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+    'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+    'parent_item_colon'  => '', // для родителей (у древовидных типов)
+    'menu_name'          => 'Отзывы', // название меню
   );
   $args = array(
+    'label'  => null,
     'labels' => $labels,
-    'has_archive' => true,
-    'public' => true,
-    'menu_icon' => 'dashicons-screenoptions',
+    'description'         => '',
+    'public'              => true,
+    'publicly_queryable'  => null,
+    'exclude_from_search' => null,
+    'show_ui'             => null,
+    'show_in_menu'        => null, // показывать ли в меню адмнки
+    'show_in_admin_bar'   => null, // по умолчанию значение show_in_menu
+    'show_in_nav_menus'   => null,
+    'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+    'rest_base'           => null, // $post_type. C WP 4.7
     'menu_position'       => 8,
-    'hierarchical' => true,
-    'supports' => array(
+    'menu_icon'           => 'dashicons-screenoptions',
+    //'capability_type'   => 'post',
+    //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+    //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+    'hierarchical'        => true,
+    'supports'            => array(
       'title',
       'thumbnail',
       'editor',
+      'page-attributes',
+      'excerpt',
+//      'custom-fields',
       'comments',
-      'page-attributes'
-    )
+      'editor',
+//      'author',
+//      'trackbacks',
+      'revisions',
+      'post-formats'
+    ),
+    'taxonomies'          => array(),
+    'has_archive'         => true,
+    'rewrite'             => true,
+    'query_var'           => true,
   );
-  register_post_type( 'news', $args );
+  register_post_type( 'review', $args );
 }
 
 ////////////////////////////////////////////////////////////////////  Taxonomy

d9781379ba0af438e8719359f68b47f2a9e0374b
Bohdan <zamchenco@gmail.com>
2017-06-21 12:12:40 +0300

    update register_taxonomy

--- includes/custom-post-types.php
+++ includes/custom-post-types.php
@@ -58,23 +58,40 @@ function post_type_review() {
 }
 
 ////////////////////////////////////////////////////////////////////  Taxonomy
-add_action( 'init', 'create_news_taxonomies', 0 );
-function create_news_taxonomies(){
-  $labels = array(
-    'name' => _x( 'Раздел', 'taxonomy general name' ),
-    'singular_name' => _x( 'Раздел', 'taxonomy singular name' ),
-    'edit_item' => __( 'Редактировать Раздел' ),
-    'update_item' => __( 'Обновить Раздел' ),
-    'add_new_item' => __( 'Добавить Раздел' ),
-    'menu_name' => __( 'Разделы' ),
-  );
-
-  // Добавляем древовидную таксономию 'genre' (как категории)
-  register_taxonomy('section', array('news'), array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'section' ),
-  ));
+add_action('init', 'create_review_category');
+function create_review_category(){
+  // список параметров: http://wp-kama.ru/function/get_taxonomy_labels
+  register_taxonomy('review_category', array('review'), array(
+    'label'                 => '', // определяется параметром $labels->name
+    'labels'                => array(
+      'name'              => 'Категории отзывов',
+      'singular_name'     => 'Категория отзыва',
+      'search_items'      => 'Поиск в категории',
+      'all_items'         => 'Все категории',
+      'parent_item'       => 'Родительськая категория',
+      'parent_item_colon' => 'Родительськая категория:',
+      'edit_item'         => 'Редактировать категорию',
+      'update_item'       => 'Обновить категорию',
+      'add_new_item'      => 'Добавить новую категорию',
+      'new_item_name'     => 'Название новой категории',
+      'menu_name'         => 'Категории отзывов',
+    ),
+    'description'           => '', // описание таксономии
+    'public'                => true,
+    'publicly_queryable'    => null, // равен аргументу public
+    'show_in_nav_menus'     => true, // равен аргументу public
+    'show_ui'               => true, // равен аргументу public
+    'show_tagcloud'         => true, // равен аргументу show_ui
+    'show_in_rest'          => null, // добавить в REST API
+    'rest_base'             => null, // $taxonomy
+    'hierarchical'          => false,
+    'update_count_callback' => '',
+    'rewrite'               => true,
+    //'query_var'             => $taxonomy, // название параметра запроса
+    'capabilities'          => array(),
+    'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
+    'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
+    '_builtin'              => false,
+    'show_in_quick_edit'    => null, // по умолчанию значение show_ui
+  ) );
 }
